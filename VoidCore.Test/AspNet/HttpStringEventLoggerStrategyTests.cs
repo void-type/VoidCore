2a91161e2debeb8032eb8b387979d6c30c6ed9b4
Jeff Schreiner <Schreinerj@prpa.org>
2018-09-04 09:49:10 -0600

    Add test for validating derived classes.

--- VoidCore.Test/AspNet/HttpStringEventLoggerStrategyTests.cs
+++ VoidCore.Test/AspNet/HttpStringEventLoggerStrategyTests.cs
@@ -40,5 +40,33 @@ namespace VoidCore.Test.AspNet
             Assert.Contains(expectedPrefix, logText);
             Assert.Contains(expectedPayload, logText);
         }
+
+        [Fact]
+        public void LogEventWithNullExceptions()
+        {
+            var httpRequestMock = new Mock<HttpRequest>();
+            httpRequestMock.Setup(request => request.Method).Returns("GET");
+            httpRequestMock.Setup(request => request.Path).Returns(new PathString("/path/to/here"));
+
+            var httpContextMock = new Mock<HttpContext>();
+            httpContextMock.Setup(context => context.Request).Returns(httpRequestMock.Object);
+            httpContextMock.Setup(context => context.TraceIdentifier).Returns("identifier");
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(accessor => accessor.HttpContext).Returns(httpContextMock.Object);
+
+            var currentUser = new Mock<ICurrentUser>();
+            currentUser.Setup(fmt => fmt.Name).Returns("userName");
+
+            var strategy = new HttpStringEventLoggerStrategy(httpContextAccessorMock.Object, currentUser.Object);
+
+            var logText = strategy.LogEvent((Exception)null, "added12", "added23");
+
+            var expectedPrefix = "identifier:userName:GET:/path/to/here";
+            var expectedPayload = "added12 added23";
+
+            Assert.Contains(expectedPrefix, logText);
+            Assert.Contains(expectedPayload, logText);
+        }
     }
 }
