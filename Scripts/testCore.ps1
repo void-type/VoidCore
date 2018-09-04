1015ad6b88c3d7058a5d65e29f19aecd1cb2d5a3
Jeff Schreiner <Schreinerj@prpa.org>
2018-09-04 09:48:49 -0600

    Add stop to test script.

--- Scripts/testCore.ps1
+++ Scripts/testCore.ps1
@@ -1,5 +1,12 @@
+function Stop-OnError {
+  if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+  }
+}
+
 Push-Location -Path "../VoidCore.Test"
 dotnet test -p:CollectCoverage=true -p:CoverletOutputFormat=opencover /p:CoverletOutput="./coverage.opencover.xml"
+Stop-OnError
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.0.0-rc4
 reportgenerator "-reports:coverage.opencover.xml" "-targetdir:coveragereport"
 Pop-Location
