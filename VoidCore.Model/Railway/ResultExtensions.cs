9c46afa1bdea6ab4063efcd88e196ff37cdcd86d
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-09-04 23:24:20 -0600

    Result extensions

---
+++ VoidCore.Model/Railway/ResultExtensions.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VoidCore.Model.Railway
+{
+    /// <summary>
+    /// Extensions for the Result class
+    /// </summary>
+    public static class ResultExtensions
+    {
+        /// <summary>
+        /// Combine many untyped results to one untyped result.
+        /// </summary>
+        /// <param name="results">The results to combine</param>
+        /// <returns>A combined result</returns>
+        public static Result Combine(this IEnumerable<Result> results)
+        {
+            return Result.Combine(results.ToArray());
+        }
+
+        /// <summary>
+        /// Combine many typed results to an untyped result.
+        /// </summary>
+        /// <param name="results">The results to combine</param>
+        /// <typeparam name="TValue">The type of value in the results</typeparam>
+        /// <returns>A combined result</returns>
+        public static Result Combine<TValue>(this IEnumerable<Result<TValue>> results)
+        {
+            return Result.Combine(results.ToArray());
+        }
+    }
+}
