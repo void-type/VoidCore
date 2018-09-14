6a2e726f54c08c82dcefda04f33ca54c3017a493
Jeff Schreiner <Schreinerj@prpa.org>
2018-09-14 14:02:57 -0600

    Remove coverlet and make a global tool.

--- Scripts/installAndUpdateTools.ps1
+++ Scripts/installAndUpdateTools.ps1
@@ -1,7 +1,10 @@
 dotnet tool install --global dotnet-outdated
+# Waiting on stable version
 dotnet tool install --global dotnet-reportgenerator-globaltool --version "4.0.0-rc4"
 dotnet tool install --global InheritDocTool
+dotnet tool install --global coverlet.console
 
 dotnet tool update --global dotnet-outdated
-# dotnet tool update --global dotnet-reportgenerator-globaltool
+dotnet tool update --global dotnet-reportgenerator-globaltool
 dotnet tool update --global InheritDocTool
+dotnet tool update --global coverlet.console
