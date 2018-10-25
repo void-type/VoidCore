f189f127d76cc6596b5b949577da968823f84176
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-10-24 20:45:00 -0600

    Make HttpContextCurrentUser a singleton.

--- VoidCore.AspNet/ClientApp/HttpContextCurrentUser.cs
+++ VoidCore.AspNet/ClientApp/HttpContextCurrentUser.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using VoidCore.Model.ClientApp;
 
 namespace VoidCore.AspNet.ClientApp
@@ -11,11 +13,14 @@ namespace VoidCore.AspNet.ClientApp
     /// </summary>
     public class HttpContextCurrentUser : ICurrentUser
     {
-        /// <inheritdoc/>
-        public string Name { get; }
 
         /// <inheritdoc/>
-        public IEnumerable<string> AuthorizedAs { get; }
+        public string Name => _userNameFormatter.Format(_user.Identity.Name);
+
+        /// <inheritdoc/>
+        public IEnumerable<string> AuthorizedAs => _applicationSettings.AuthorizationPolicies
+            .Where(policy => _authorizationService.AuthorizeAsync(_user, policy.Key).Result.Succeeded)
+            .Select(policy => policy.Key);
 
         /// <summary>
         /// Create a new current user accessor
@@ -26,13 +31,16 @@ namespace VoidCore.AspNet.ClientApp
         /// <param name="applicationSettings">The application's authorization settings</param>
         public HttpContextCurrentUser(IHttpContextAccessor httpContextAccessor, IUserNameFormatStrategy userNameFormatter, IAuthorizationService authorizationService, IApplicationSettings applicationSettings)
         {
-            var user = httpContextAccessor.HttpContext.User;
-
-            Name = userNameFormatter.Format(user.Identity.Name);
-
-            AuthorizedAs = applicationSettings.AuthorizationPolicies
-                .Where(policy => authorizationService.AuthorizeAsync(user, policy.Key).Result.Succeeded)
-                .Select(policy => policy.Key);
+            _httpContextAccessor = httpContextAccessor;
+            _userNameFormatter = userNameFormatter;
+            _authorizationService = authorizationService;
+            _applicationSettings = applicationSettings;
         }
+
+        private ClaimsPrincipal _user => _httpContextAccessor.HttpContext.User;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserNameFormatStrategy _userNameFormatter;
+        private readonly IAuthorizationService _authorizationService;
+        private readonly IApplicationSettings _applicationSettings;
     }
 }
