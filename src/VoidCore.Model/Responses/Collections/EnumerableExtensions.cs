25bc1a0428f995ab4aeca5f892064d6663c5de56
Jeff Schreiner <Schreinerj@prpa.org>
2019-03-11 14:26:42 -0600

    Cleanup ItemSet ctors

--- src/VoidCore.Model/Responses/Collections/EnumerableExtensions.cs
+++ src/VoidCore.Model/Responses/Collections/EnumerableExtensions.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 namespace VoidCore.Model.Responses.Collections
 {
     /// <summary>
-    /// Helpers for finalizing queries and transforming them into view DTOs.
+    /// Extensions to make ItemSets from enumerable collections.
     /// </summary>
     public static class EnumerableExtensions
     {
         /// <summary>
-        /// Create a new ItemSet. Note that this will finalize deferred queries.
+        /// Create an item set without pagination.
         /// </summary>
         /// <param name="items">The items to return in the set</param>
         /// <typeparam name="T">The type of entities in the collection</typeparam>
@@ -19,31 +19,32 @@ namespace VoidCore.Model.Responses.Collections
         }
 
         /// <summary>
-        /// Create a page from an entire item set.
+        /// Create a paged item set with option to turn off pagination.
         /// </summary>
         /// <param name="items">The full set of items</param>
         /// <param name="page">What page number to take from the set</param>
         /// <param name="take">How many items to include in each page</param>
+        /// <param name="isPagingEnabled">If paging is enabled in this set. Default of pagination enabled</param>
         /// <typeparam name="T">The type of entities in the collection</typeparam>
         /// <returns>A new ItemSet of these items</returns>
-        public static IItemSet<T> ToItemSet<T>(this IEnumerable<T> items, int page, int take)
+        public static IItemSet<T> ToItemSet<T>(this IEnumerable<T> items, int page, int take, bool isPagingEnabled = true)
         {
-            return new ItemSet<T>(items, page, take);
+            return new ItemSet<T>(items, page, take, isPagingEnabled);
         }
 
         /// <summary>
-        /// Create an item set from an already paginated set.
+        /// Create an item set from explicit properties. This extension will not perform pagination.
         /// </summary>
-        /// <param name="pageItems">The page from set of items</param>
+        /// <param name="items">The page from set of items</param>
         /// <param name="page">What page number to take from the set</param>
         /// <param name="take">How many items to include in each page</param>
         /// <param name="totalCount">The count of the whole set before it was paginated</param>
         /// <param name="isPagingEnabled">If paging is enabled in this set</param>
         /// <typeparam name="T">The type of entities in the collection</typeparam>
         /// <returns>A new ItemSet of these items</returns>
-        public static IItemSet<T> ToItemSet<T>(this IEnumerable<T> pageItems, int page, int take, int totalCount, bool isPagingEnabled = true)
+        public static IItemSet<T> ToItemSet<T>(this IEnumerable<T> items, int totalCount, bool isPagingEnabled, int page, int take)
         {
-            return new ItemSet<T>(pageItems, page, take, totalCount, isPagingEnabled);
+            return new ItemSet<T>(items, totalCount, isPagingEnabled, page, take);
         }
     }
 }
--- src/VoidCore.Model/Responses/Collections/ItemSet.cs
+++ src/VoidCore.Model/Responses/Collections/ItemSet.cs
@@ -11,30 +11,21 @@ namespace VoidCore.Model.Responses.Collections
     public class ItemSet<T> : IItemSet<T>
     {
         /// <summary>
-        /// Create a non-paged item set.
+        /// Create an item set without pagination.
         /// </summary>
         /// <param name="items">The full set of items</param>
         /// <exception cref="ArgumentNullException">Throws an ArgumentNullException if null is passed for items.</exception>
-        public ItemSet(IEnumerable<T> items)
-        {
-            if (items == null)
-            {
-                throw new ArgumentNullException(nameof(items), "Cannot make an ItemSet of null items.");
-            }
-
-            var itemsList = items.ToList();
-            TotalCount = itemsList.Count();
-            Items = itemsList;
-        }
+        public ItemSet(IEnumerable<T> items) : this(items, 0, 0, false) { }
 
         /// <summary>
-        /// Create a page from an entire item set.
+        /// Create a paged item set with option to turn off pagination.
         /// </summary>
         /// <param name="items">The full set of items</param>
         /// <param name="page">What page number to take from the set</param>
         /// <param name="take">How many items to include in each page</param>
+        /// <param name="isPagingEnabled">If paging is enabled in this set. Default of pagination enabled</param>
         /// <exception cref="ArgumentNullException">Throws an ArgumentNullException if null is passed for items.</exception>
-        public ItemSet(IEnumerable<T> items, int page, int take)
+        public ItemSet(IEnumerable<T> items, int page, int take, bool isPagingEnabled = true)
         {
             if (items == null)
             {
@@ -42,37 +33,45 @@ namespace VoidCore.Model.Responses.Collections
             }
 
             var allItems = items.ToList();
+            IsPagingEnabled = isPagingEnabled;
 
-            IsPagingEnabled = true;
-            Page = page;
-            Take = take;
-            TotalCount = allItems.Count();
-            Items = allItems
-                .Skip((page - 1) * take)
-                .Take(take);
+            if (isPagingEnabled)
+            {
+                Page = page;
+                Take = take;
+                TotalCount = allItems.Count();
+                Items = allItems
+                    .Skip((page - 1) * take)
+                    .Take(take);
+            }
+            else
+            {
+                TotalCount = allItems.Count();
+                Items = allItems;
+            }
         }
 
         /// <summary>
         /// Create an item set from explicit properties. This constructor will not perform pagination.
         /// </summary>
-        /// <param name="pageItems">The page from set of items</param>
-        /// <param name="page">What page number to take from the set</param>
-        /// <param name="take">How many items to include in each page</param>
+        /// <param name="items">The page from set of items</param>
         /// <param name="totalCount">The count of the whole set before it was paginated</param>
         /// <param name="isPagingEnabled">Mark the set as paged or not paged</param>
+        /// <param name="page">What page number to take from the set</param>
+        /// <param name="take">How many items to include in each page</param>
         /// <exception cref="ArgumentNullException">Throws an ArgumentNullException if null is passed for items.</exception>
-        public ItemSet(IEnumerable<T> pageItems, int page, int take, int totalCount, bool isPagingEnabled = true)
+        public ItemSet(IEnumerable<T> items, int totalCount, bool isPagingEnabled, int page, int take)
         {
-            if (pageItems == null)
+            if (items == null)
             {
-                throw new ArgumentNullException(nameof(pageItems), "Cannot make an ItemSet of null items.");
+                throw new ArgumentNullException(nameof(items), "Cannot make an ItemSet of null items.");
             }
 
             IsPagingEnabled = isPagingEnabled;
             Page = page;
             Take = take;
             TotalCount = totalCount;
-            Items = pageItems.ToList();
+            Items = items.ToList();
         }
 
         /// <inheritdoc/>
