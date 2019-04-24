d8505923bca5c77327e31e0438d82ea8e638ada6
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2019-04-23 22:19:53 -0600

    Tests for email user name strategy

--- tests/VoidCore.Test/AspNet/Auth/UserNameFormatStrategyTests.cs
+++ tests/VoidCore.Test/AspNet/Auth/UserNameFormatStrategyTests.cs
@@ -1,7 +1,7 @@
 using VoidCore.AspNet.Auth;
 using Xunit;
 
-namespace VoidCore.Test.AspNet.Users
+namespace VoidCore.Test.AspNet.Auth
 {
     public class UserNameFormatStrategyTests
     {
@@ -12,6 +12,17 @@ namespace VoidCore.Test.AspNet.Users
         public void FormatNameFromAdLogin(string input, string expected)
         {
             var formatter = new AdLoginUserNameFormatStrategy();
+            var userName = formatter.Format(input);
+            Assert.Equal(expected, userName);
+        }
+
+        [Theory]
+        [InlineData("Name@contoso.com", "Name")]
+        [InlineData("Name", "Name")]
+        [InlineData(null, "Unknown")]
+        public void FormatNameFromEmail(string input, string expected)
+        {
+            var formatter = new EmailUserNameFormatStrategy();
             var userName = formatter.Format(input);
             Assert.Equal(expected, userName);
         }
