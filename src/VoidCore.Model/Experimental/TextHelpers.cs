57a17a99fd024991c888ad880112ab5a632590dc
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2019-11-20 23:15:01 -0700

    Experimental TextHelpers.

---
+++ src/VoidCore.Model/Experimental/TextHelpers.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Linq;
+
+#warning This namespace contains experimental features that are subject to change outside of semver.
+namespace VoidCore.Model.Experimental
+{
+    // TODO: these are experimental
+    /// <summary>
+    /// Helpers useful for logging or emailing.
+    /// </summary>
+    public static class TextHelpers
+    {
+        private const string _dateFormat = "s";
+
+        /// <summary>
+        /// Split a string on any newline character.
+        /// </summary>
+        /// <param name="str">The string to split</param>
+        public static string[] SplitOnNewLine(string str)
+        {
+            return str.Split(new[] { "\n", "\r\n" }, StringSplitOptions.None);
+        }
+
+        /// <summary>
+        /// Make an anchor tag string.
+        /// </summary>
+        /// <param name="caption">The text shown to the user</param>
+        /// <param name="urlSegments">A series of url segments to be joined with "/"</param>
+        /// <returns></returns>
+        public static string Link(string caption, params string[] urlSegments)
+        {
+            return $"<a href=\"{string.Join("/", urlSegments)}\">{caption}</a>";
+        }
+
+        /// <summary>
+        /// Print a DateTime to a string.
+        /// </summary>
+        /// <param name="dateTime">The date to format</param>
+        /// <param name="dateFormat">The date format. ISO 8601 by default</param>
+        /// <returns></returns>
+        public static string Print(DateTime dateTime, string dateFormat = _dateFormat)
+        {
+            dateFormat ??= "s";
+            return dateTime.ToString(dateFormat, CultureInfo.InvariantCulture);
+        }
+
+
+        /// <summary>
+        /// Print an enumerable to a string.
+        /// </summary>
+        /// <param name="strings"></param>
+        /// <returns></returns>
+        public static string Print(IEnumerable<string> strings)
+        {
+            return string.Join(", ", strings);
+        }
+
+        /// <summary>
+        /// Print an object to a string. Iterates properties to print "Name: Value"
+        /// </summary>
+        /// <param name="obj">The object to print</param>
+        /// <param name="dateFormat">The date format. ISO 8601 by default</param>
+        public static IEnumerable<string> PrintObject(object obj, string dateFormat = _dateFormat)
+        {
+            foreach (var property in obj.GetType().GetTypeInfo().GetProperties())
+            {
+                var value = property.GetValue(obj);
+
+                switch (value)
+                {
+                    case string str:
+                        yield return $"{property.Name}: {str}";
+                        break;
+
+                    case DateTime dateTime:
+                        yield return $"{property.Name}: {Print(dateTime, dateFormat)}";
+                        break;
+
+                    case IEnumerable<DateTime> dateTimes:
+                        yield return $"{property.Name}: {Print(dateTimes.Select(d => Print(d, dateFormat)))}";
+                        break;
+
+                    case IEnumerable items:
+                        List<string> strings = new List<string>();
+                        foreach (var item in items)
+                        {
+                            strings.Add(item.ToString());
+                        }
+
+                        yield return $"{property.Name}: {Print(strings)}";
+                        break;
+
+                    default:
+                        yield return $"{property.Name}: {value.ToString()}";
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ tests/VoidCore.Test/Model/Experimental/TextHelpersTests.cs
@@ -0,0 +1,70 @@
+using System;
+using VoidCore.Model.Experimental;
+using Xunit;
+
+namespace VoidCore.Test.Model.Experimental
+{
+    public class TextHelpersTests
+    {
+        [Fact]
+        public void Can_make_lines_from_object_properties_with_dates_and_arrays()
+        {
+            var strings = TextHelpers.PrintObject(new TestObject(), "MM/dd/yyyy h:mm tt");
+
+            Assert.Equal(new[]{
+                "MyString: Hello World",
+                "MyInteger: 201",
+                "MyStrings: 5, 6, 7, 8",
+                "MyIntegers: 1, 2, 3, 4",
+                "MyDateTime: 12/01/2018 9:09 AM",
+                "MyDate: 09/10/2008 12:00 AM",
+                "MyDateTimes: 09/10/2008 12:00 AM, 12/01/2018 9:09 AM, 09/10/2008 12:00 AM",
+
+                }, strings);
+        }
+
+        [Fact]
+        public void Can_format_dates_to_ISO8601_by_default()
+        {
+            var strings = TextHelpers.PrintObject(new TestObject());
+
+            Assert.Equal(new[]{
+                "MyString: Hello World",
+                "MyInteger: 201",
+                "MyStrings: 5, 6, 7, 8",
+                "MyIntegers: 1, 2, 3, 4",
+                "MyDateTime: 2018-12-01T09:09:08",
+                "MyDate: 2008-09-10T00:00:00",
+                "MyDateTimes: 2008-09-10T00:00:00, 2018-12-01T09:09:08, 2008-09-10T00:00:00",
+
+                }, strings);
+        }
+
+        [Fact]
+        public void Can_make_web_links()
+        {
+            var link = TextHelpers.Link("Click me", "https://www.contoso.com", "recipes", "1");
+
+            Assert.Equal("<a href=\"https://www.contoso.com/recipes/1\">Click me</a>", link);
+        }
+
+        [Fact]
+        public void Can_split_text_on_any_new_line()
+        {
+            var strings = TextHelpers.SplitOnNewLine("Click me\nhttps://www.contoso.com\r\nrecipes\\1");
+
+            Assert.Equal(new[] { "Click me", "https://www.contoso.com", "recipes\\1" }, strings);
+        }
+
+        public class TestObject
+        {
+            public string MyString { get; set; } = "Hello World";
+            public int MyInteger { get; set; } = 201;
+            public string[] MyStrings { get; set; } = new[] { "5", "6", "7", "8" };
+            public int[] MyIntegers { get; set; } = new[] { 1, 2, 3, 4 };
+            public DateTime MyDateTime { get; set; } = new DateTime(2018, 12, 1, 9, 9, 8);
+            public DateTime MyDate { get; set; } = new DateTime(2008, 9, 10);
+            public DateTime[] MyDateTimes { get; set; } = new[] { new DateTime(2008, 9, 10), new DateTime(2018, 12, 1, 9, 9, 8), new DateTime(2008, 9, 10) };
+        }
+    }
+}
