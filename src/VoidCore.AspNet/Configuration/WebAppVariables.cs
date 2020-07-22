9b6482d577b7f8e2fd1de1004bea1916dec4f8e3
Jeff Schreiner <Schreinerj@prpa.org>
2020-07-22 09:05:28 -0600

    Breaking: Remove BaseUrl auto-resolution.

--- src/VoidCore.AspNet/Configuration/WebAppVariables.cs
+++ src/VoidCore.AspNet/Configuration/WebAppVariables.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using VoidCore.Domain.Guards;
+﻿using Microsoft.Extensions.Hosting;
 using VoidCore.Model.Configuration;
 
 namespace VoidCore.AspNet.Configuration
@@ -8,19 +6,16 @@ namespace VoidCore.AspNet.Configuration
     /// <inheritdoc/>
     public class WebAppVariables : IWebAppVariables
     {
-        private readonly IHttpContextAccessor _contextAccessor;
         private readonly ApplicationSettings _appSettings;
         private readonly IHostEnvironment _host;
 
         /// <summary>
         /// Construct a new WebAppVariables object.
         /// </summary>
-        /// <param name="contextAccessor">The HttpContextAccessor</param>
         /// <param name="host">The host environment</param>
         /// <param name="appSettings">The application settings</param>
-        public WebAppVariables(IHttpContextAccessor contextAccessor, IHostEnvironment host, ApplicationSettings appSettings)
+        public WebAppVariables(IHostEnvironment host, ApplicationSettings appSettings)
         {
-            _contextAccessor = contextAccessor;
             _appSettings = appSettings;
             _host = host;
         }
@@ -30,23 +25,7 @@ namespace VoidCore.AspNet.Configuration
 
         /// <inheritdoc/>
 
-        public string BaseUrl
-        {
-#warning BaseUrl will no longer auto-resolve from HttpContext in future versions. You must configure BaseUrl in ApplicationSettings. This is to prevent misuse and Host Header injection.
-            get
-            {
-                if (string.IsNullOrWhiteSpace(_appSettings.BaseUrl))
-                {
-                    // For apps that don't have the BaseUrl in settings, we can calculate it from the request.
-                    // This method does not work for hosted services that don't have a web request.
-                    _contextAccessor.HttpContext.EnsureNotNull(nameof(_contextAccessor.HttpContext));
-                    var request = _contextAccessor.HttpContext.Request;
-                    return $"{request.Scheme}://{request.Host}{request.PathBase}";
-                }
-
-                return _appSettings.BaseUrl;
-            }
-        }
+        public string BaseUrl => _appSettings.BaseUrl;
 
         /// <inheritdoc/>
         public string Environment => _host.EnvironmentName;
--- tests/VoidCore.Test/AspNet/Configuration/WebAppVariablesTests.cs
+++ tests/VoidCore.Test/AspNet/Configuration/WebAppVariablesTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Hosting;
 using Moq;
 using VoidCore.AspNet.Configuration;
 using Xunit;
@@ -13,49 +12,10 @@ namespace VoidCore.Test.AspNet.Configuration
         {
             var appSettings = new ApplicationSettings("AppName", "https://www.contoso.com/path/base");
 
-            var httpRequestMock = new Mock<HttpRequest>();
-
-            var httpContextMock = new Mock<HttpContext>();
-
-            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-            httpContextAccessorMock.Setup(accessor => accessor.HttpContext)
-                .Returns(httpContextMock.Object);
-
             var hostMock = new Mock<IHostEnvironment>();
             hostMock.Setup(h => h.EnvironmentName).Returns("TestingEnvironment");
 
-            var variables = new WebAppVariables(httpContextAccessorMock.Object, hostMock.Object, appSettings);
-
-            Assert.Equal("AppName", variables.AppName);
-            Assert.Equal("https://www.contoso.com/path/base", variables.BaseUrl);
-            Assert.Equal("TestingEnvironment", variables.Environment);
-        }
-
-        [Fact]
-        public void BaseUrl_returns_httpContext_url_when_settings_empty()
-        {
-            var appSettings = new ApplicationSettings("AppName", "");
-
-            var httpRequestMock = new Mock<HttpRequest>();
-            httpRequestMock.Setup(request => request.Scheme)
-                .Returns("https");
-            httpRequestMock.Setup(request => request.Host)
-                .Returns(new HostString("www.contoso.com"));
-            httpRequestMock.Setup(request => request.PathBase)
-                .Returns(new PathString("/path/base"));
-
-            var httpContextMock = new Mock<HttpContext>();
-            httpContextMock.Setup(context => context.Request)
-                .Returns(httpRequestMock.Object);
-
-            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-            httpContextAccessorMock.Setup(accessor => accessor.HttpContext)
-                .Returns(httpContextMock.Object);
-
-            var hostMock = new Mock<IHostEnvironment>();
-            hostMock.Setup(h => h.EnvironmentName).Returns("TestingEnvironment");
-
-            var variables = new WebAppVariables(httpContextAccessorMock.Object, hostMock.Object, appSettings);
+            var variables = new WebAppVariables(hostMock.Object, appSettings);
 
             Assert.Equal("AppName", variables.AppName);
             Assert.Equal("https://www.contoso.com/path/base", variables.BaseUrl);
