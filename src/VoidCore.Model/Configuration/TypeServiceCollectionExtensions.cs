ad3fbd8da057bc5da7336407cb5795d5b9afc4fa
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2022-09-29 23:19:20 -0600

    Can add services by service, in addition to implementation

--- src/VoidCore.Model/Configuration/TypeServiceCollectionExtensions.cs
+++ src/VoidCore.Model/Configuration/TypeServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using VoidCore.Model.Events;
@@ -14,7 +15,7 @@ namespace VoidCore.Model.Configuration;
 public static class TypeServiceCollectionExtensions
 {
     /// <summary>
-    /// Search assemblies for domain event components and registers them with the DI container by concrete name.
+    /// Search assemblies for domain event components and registers them with the DI container by implementation.
     /// This is a convenience method that has a cost of uniformity. All components are registered with the specified lifetime.
     /// You can override how a service gets registered by re-registering it under a different lifetime manually later in Startup.cs.
     /// </summary>
@@ -33,39 +34,79 @@ public static class TypeServiceCollectionExtensions
     }
 
     /// <summary>
-    /// Search assemblies for implementations of classes and interfaces and registers them with the DI container by concrete name.
+    /// Search assemblies for implementations of classes and interfaces and registers them with the DI container by implementation.
     /// </summary>
     /// <param name="services">This service collection</param>
     /// <param name="lifetime">The ServiceLifetime to register the services for.</param>
     /// <param name="baseTypes">An array of base types or interfaces to register concrete implementations of</param>
     /// <param name="assembliesToSearch">An array of assemblies to search for domain components in</param>
+    /// <exception cref="NotImplementedException">If an unsupported service lifetime is given</exception>
     public static void AddImplementationsOfBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToSearch)
     {
         assembliesToSearch.EnsureNotNullOrEmpty(nameof(assembliesToSearch));
 
         foreach (var baseType in baseTypes)
         {
-            // DI can register concrete types using any base class, including open generics.
-            var foundConcretes = assembliesToSearch
-                .Distinct()
-                .SelectMany(assembly => assembly.DefinedTypes)
-                .Where(foundType => !foundType.IsAbstract && foundType.Implements(baseType));
-
-            foreach (var foundConcrete in foundConcretes)
+            foreach (var implementation in SearchAssembliesForTypes(assembliesToSearch, baseType))
             {
                 switch (lifetime)
                 {
                     case ServiceLifetime.Singleton:
-                        services.AddSingleton(foundConcrete);
+                        services.AddSingleton(implementation);
                         break;
                     case ServiceLifetime.Scoped:
-                        services.AddScoped(foundConcrete);
+                        services.AddScoped(implementation);
+                        break;
+                    case ServiceLifetime.Transient:
+                        services.AddTransient(implementation);
                         break;
                     default:
-                        services.AddTransient(foundConcrete);
-                        break;
+                        throw new NotImplementedException($"Service lifetime of {lifetime} not supported.");
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// Search assemblies for implementations of classes and interfaces and registers them with the DI container by base type.
+    /// </summary>
+    /// <param name="services">This service collection</param>
+    /// <param name="lifetime">The ServiceLifetime to register the services for.</param>
+    /// <param name="baseTypes">An array of base types or interfaces to register concrete implementations of</param>
+    /// <param name="assembliesToSearch">An array of assemblies to search for domain components in</param>
+    /// <exception cref="NotImplementedException">If an unsupported service lifetime is given</exception>
+    public static void AddImplementationsByBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToSearch)
+    {
+        assembliesToSearch.EnsureNotNullOrEmpty(nameof(assembliesToSearch));
+
+        foreach (var baseType in baseTypes)
+        {
+            // DI can register concrete types using any base class, including open generics.
+            foreach (var implementation in SearchAssembliesForTypes(assembliesToSearch, baseType))
+            {
+                switch (lifetime)
+                {
+                    case ServiceLifetime.Singleton:
+                        services.AddSingleton(baseType, implementation);
+                        break;
+                    case ServiceLifetime.Scoped:
+                        services.AddScoped(baseType, implementation);
+                        break;
+                    case ServiceLifetime.Transient:
+                        services.AddTransient(baseType, implementation);
+                        break;
+                    default:
+                        throw new NotImplementedException($"Service lifetime of {lifetime} not supported.");
+                }
+            }
+        }
+    }
+
+    private static IEnumerable<TypeInfo> SearchAssembliesForTypes(Assembly[] assembliesToSearch, Type baseType)
+    {
+        return assembliesToSearch
+            .Distinct()
+            .SelectMany(assembly => assembly.DefinedTypes)
+            .Where(foundType => !foundType.IsAbstract && foundType.Implements(baseType));
     }
 }
