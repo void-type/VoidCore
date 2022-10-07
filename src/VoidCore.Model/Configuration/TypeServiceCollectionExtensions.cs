e0c31dc06763af8292db15f2fff96b4ae9801ee7
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2022-10-07 00:48:00 -0600

    Change wording from assembly search to scan

--- src/VoidCore.Model/Configuration/TypeServiceCollectionExtensions.cs
+++ src/VoidCore.Model/Configuration/TypeServiceCollectionExtensions.cs
@@ -15,14 +15,14 @@ namespace VoidCore.Model.Configuration;
 public static class TypeServiceCollectionExtensions
 {
     /// <summary>
-    /// Search assemblies for domain event components and registers them with the DI container by implementation.
+    /// Scan assemblies for domain event components and registers them with the DI container by implementation.
     /// This is a convenience method that has a cost of uniformity. All components are registered with the specified lifetime.
     /// You can override how a service gets registered by re-registering it under a different lifetime manually later in Startup.cs.
     /// </summary>
     /// <param name="services">This service collection</param>
     /// <param name="lifetime">The ServiceLifetime to register the services for.</param>
-    /// <param name="assembliesToSearch">An array of assemblies to search for domain components in</param>
-    public static void AddDomainEvents(this IServiceCollection services, ServiceLifetime lifetime, params Assembly[] assembliesToSearch)
+    /// <param name="assembliesToScan">An array of assemblies to scan for domain components in</param>
+    public static void AddDomainEvents(this IServiceCollection services, ServiceLifetime lifetime, params Assembly[] assembliesToScan)
     {
         var domainEventTypes = new[] {
                 typeof (IEventHandler<,>),
@@ -30,24 +30,24 @@ public static class TypeServiceCollectionExtensions
                 typeof (IRequestValidator<>),
                 typeof (IPostProcessor<,>)};
 
-        AddImplementationsOfBaseType(services, lifetime, domainEventTypes, assembliesToSearch);
+        AddImplementationsOfBaseType(services, lifetime, domainEventTypes, assembliesToScan);
     }
 
     /// <summary>
-    /// Search assemblies for implementations of classes and interfaces and registers them with the DI container by implementation.
+    /// Scan assemblies for implementations of classes and interfaces and registers them with the DI container by implementation.
     /// </summary>
     /// <param name="services">This service collection</param>
     /// <param name="lifetime">The ServiceLifetime to register the services for.</param>
     /// <param name="baseTypes">An array of base types or interfaces to register concrete implementations of</param>
-    /// <param name="assembliesToSearch">An array of assemblies to search for domain components in</param>
+    /// <param name="assembliesToScan">An array of assemblies to scan for implementations in</param>
     /// <exception cref="NotImplementedException">If an unsupported service lifetime is given</exception>
-    public static void AddImplementationsOfBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToSearch)
+    public static void AddImplementationsOfBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToScan)
     {
-        assembliesToSearch.EnsureNotNullOrEmpty(nameof(assembliesToSearch));
+        assembliesToScan.EnsureNotNullOrEmpty(nameof(assembliesToScan));
 
         foreach (var baseType in baseTypes)
         {
-            foreach (var implementation in SearchAssembliesForTypes(assembliesToSearch, baseType))
+            foreach (var implementation in ScanAssembliesForTypes(assembliesToScan, baseType))
             {
                 switch (lifetime)
                 {
@@ -68,21 +68,21 @@ public static class TypeServiceCollectionExtensions
     }
 
     /// <summary>
-    /// Search assemblies for implementations of classes and interfaces and registers them with the DI container by base type.
+    /// Scan assemblies for implementations of classes and interfaces and registers them with the DI container by base type.
     /// </summary>
     /// <param name="services">This service collection</param>
     /// <param name="lifetime">The ServiceLifetime to register the services for.</param>
     /// <param name="baseTypes">An array of base types or interfaces to register concrete implementations of</param>
-    /// <param name="assembliesToSearch">An array of assemblies to search for domain components in</param>
+    /// <param name="assembliesToScan">An array of assemblies to scan for implementations in</param>
     /// <exception cref="NotImplementedException">If an unsupported service lifetime is given</exception>
-    public static void AddImplementationsByBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToSearch)
+    public static void AddImplementationsOfBaseTypeAsBaseType(this IServiceCollection services, ServiceLifetime lifetime, Type[] baseTypes, params Assembly[] assembliesToScan)
     {
-        assembliesToSearch.EnsureNotNullOrEmpty(nameof(assembliesToSearch));
+        assembliesToScan.EnsureNotNullOrEmpty(nameof(assembliesToScan));
 
         foreach (var baseType in baseTypes)
         {
             // DI can register concrete types using any base class, including open generics.
-            foreach (var implementation in SearchAssembliesForTypes(assembliesToSearch, baseType))
+            foreach (var implementation in ScanAssembliesForTypes(assembliesToScan, baseType))
             {
                 switch (lifetime)
                 {
@@ -102,9 +102,9 @@ public static class TypeServiceCollectionExtensions
         }
     }
 
-    private static IEnumerable<TypeInfo> SearchAssembliesForTypes(Assembly[] assembliesToSearch, Type baseType)
+    private static IEnumerable<TypeInfo> ScanAssembliesForTypes(Assembly[] assembliesToScan, Type baseType)
     {
-        return assembliesToSearch
+        return assembliesToScan
             .Distinct()
             .SelectMany(assembly => assembly.DefinedTypes)
             .Where(foundType => !foundType.IsAbstract && foundType.Implements(baseType));
