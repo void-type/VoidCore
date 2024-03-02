bd9101e007bf451ab4d540e4147ee54a74e4a253
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2024-03-01 21:45:36 -0700

    Add EF queryable ToItemSet extension

---
+++ src/VoidCore.EntityFramework/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VoidCore.Model.Responses.Collections;
+
+namespace VoidCore.EntityFramework;
+
+/// <summary>
+/// Extensions for Queryables.
+/// </summary>
+public static class QueryableExtensions
+{
+    /// <summary>
+    /// Query for a paginated ItemSet. This will query the total count and the requested page of entities.
+    /// </summary>
+    /// <typeparam name="T">The type of entities to retrieve</typeparam>
+    /// <param name="query">The base query. Filtered and sorted, but don't use skip or take yet.</param>
+    /// <param name="paginationOptions">Options to control pagination</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    public static async Task<IItemSet<T>> ToItemSet<T>(this IQueryable<T> query, PaginationOptions paginationOptions, CancellationToken cancellationToken)
+    {
+        var totalCount = await query.CountAsync(cancellationToken);
+
+        var items = await query
+            .GetPage(paginationOptions)
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+
+        return items.ToItemSet(paginationOptions, totalCount);
+    }
+}
---
+++ tests/VoidCore.Test/EfIntegration/EfExtensionTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using VoidCore.EntityFramework;
+using VoidCore.Model.Responses.Collections;
+using VoidCore.Test.EfIntegration.TestModels;
+using Xunit;
+
+namespace VoidCore.Test.EfIntegration;
+
+public class EfExtensionTests
+{
+    [Fact]
+    public async Task Get_page_of_results_using_ToItemSet_extension()
+    {
+        using var context = Deps.FoodStuffsContext().Seed();
+
+        var paginationOptions = new PaginationOptions(2, 1);
+
+        var page = await context.Recipe
+            .OrderBy(x => x.Name)
+            .ToItemSet(paginationOptions, CancellationToken.None);
+
+        Assert.Equal(3, page.TotalCount);
+        Assert.Equal("Recipe2", page.Items.Single().Name);
+    }
+
+    [Fact]
+    public async Task Get_page_of_all_results_using_ToItemSet_extension()
+    {
+        using var context = Deps.FoodStuffsContext().Seed();
+
+        var paginationOptions = PaginationOptions.None;
+
+        var page = await context.Recipe
+            .OrderBy(x => x.Name)
+            .ToItemSet(paginationOptions, CancellationToken.None);
+
+        Assert.Equal(3, page.TotalCount);
+        Assert.Equal(3, page.Items.Count());
+    }
+}
